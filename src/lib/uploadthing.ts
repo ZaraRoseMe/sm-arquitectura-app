2dd005c963defdc077f7132bee5f43d660dd96ae
ZaraRoseMe <zaravdelarosa@gmail.com>
2026-05-07 00:26:38 -0700

    Create uploadthing.ts

---
+++ src/lib/uploadthing.ts
@@ -0,0 +1,7 @@
+// src/lib/uploadthing.ts
+import { generateUploadButton, generateUploadDropzone, generateReactHelpers } from '@uploadthing/react'
+import type { OurFileRouter } from '@/app/api/uploadthing/core'
+
+export const { useUploadThing, uploadFiles } = generateReactHelpers<OurFileRouter>()
+export const UploadButton = generateUploadButton<OurFileRouter>()
+export const UploadDropzone = generateUploadDropzone<OurFileRouter>()
